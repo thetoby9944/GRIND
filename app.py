1f8cc63847e66754c8ff86f170be45f9366e5cb7
Tobias Schiele <thetoby@web.de>
2022-02-06 23:36:46 +0100

    Footer

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 import pandas as pd
+from footer import footer
 
 st.set_page_config(
     page_title="GRIND.",
-    page_icon="assets/grind_logo.png"
+    page_icon="assets/grind_icon.png"
 )
 
 st.image("assets/grind_logo.svg")
-
 df = pd.read_csv(
     r"https://docs.google.com/spreadsheets/d/1qS66r_5lfu6xgUGDaExV6gaMoZO62i-yczodugfvNdY/export?format=csv&gid=0"
 )
-
 
 for col in df.columns[:-1][::-1]:
     filter_by = st.multiselect(
@@ -23,9 +22,10 @@ for col in df.columns[:-1][::-1]:
 st.write("# ")
 st.write("Your exercise of the day is:")
 
-exercise_placeholder = st.empty()
 exercise = df.sample().Exercise.values[0]
 if st.button("⟳"):
     exercise = df.sample().Exercise.values[0]
 
-exercise_placeholder.write(f"# {exercise}")
+st.write(f"# {exercise}")
+
+footer()

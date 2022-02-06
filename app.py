9be18c137791e81f6f05a6f943e08b5407596e08
Tobias Schiele <thetoby@web.de>
2022-02-06 22:34:46 +0100

    Random Exercise Feature

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="GRIND.",
+    page_icon="assets/grind_logo.png"
+)
+
+st.image("assets/grind_logo.svg")
+
+df = pd.read_csv(
+    r"https://docs.google.com/spreadsheets/d/1qS66r_5lfu6xgUGDaExV6gaMoZO62i-yczodugfvNdY/export?format=csv&gid=0"
+)
+
+
+for col in df.columns[:-1][::-1]:
+    filter_by = st.multiselect(
+        f"Filter by {col}",
+        df[col].unique()
+    )
+    if len(filter_by):
+        df = df[df[col].isin(filter_by)]
+st.write("# ")
+st.write("Your exercise of the day is:")
+st.write(f"# {df.sample().Exercise.values[0]}")
